23c4ffc0661a253886fc660f0a4a4f6438a1f2ae
Pavan kumar Kulkarni <pavankumar.kulkarni@gmail.com>
2021-03-21 13:47:14 -0400

    profile section update

--- src/Components/Summary/Summary.js
+++ src/Components/Summary/Summary.js
@@ -1,17 +1,26 @@
 import img from "../Pavan.jfif";
+import style from "./Summary.module.css";
+import myPic from "../profile.png";
 const Maincontent = () => {
   return (
     <div className="main">
-      <div>
-        <img className="myPic" src={img} alt="" />
-      </div>
-      <div>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda
-          aperiam tenetur adipisci laborum provident, aspernatur quae ex nulla
-          labore. Suscipit, distinctio? Voluptas corrupti reiciendis architecto
-          fugit quo nulla deleniti at!
-        </p>
+      <div className={style.card}>
+        <h3>Pavan Kumar Kulkarni</h3>
+        <div className={style.cardBody}>
+          <article className={style.cardBody_text}>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut,
+              impedit ut eos, ullam facilis hic explicabo exercitationem unde
+              libero atque odio maiores aliquam ab esse praesentium molestias
+              natus delectus non?
+            </p>
+            {/* <button type="button">Projects</button> */}
+            <a href="#work">Projects</a>
+          </article>
+          <article className={style.cardBody_image}>
+            <img src={myPic} alt="Pavan" />
+          </article>
+        </div>
       </div>
     </div>
   );
